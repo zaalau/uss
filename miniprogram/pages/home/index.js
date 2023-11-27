1ab38a9a45bd571bec6a4339c4e2f32f0fcd771c
zaalau <zaalauyz@gmail.com>
2023-11-28 02:10:26 +0800

    fir

---
+++ miniprogram/pages/home/index.js
@@ -0,0 +1,114 @@
+// pages/home/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    content:'今天我们一起吃了这路也，那是我最爱吃的这路也，知道吗，喝喝我很愤怒，我们还是吵架了，一天可以吵架八千次，不过我还能靠写页面消解，恩恩我就是个废物，其实我很想去洗澡上去和你睡觉了，想喝酒。。enen-/-',
+    showTextarea:false
+  },
+  setTextarea() {
+    this.setData({
+      showTextarea: !this.data.showTextarea
+    })
+  },
+  setContent(e) {
+    this.setData({
+      content:e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    const res = wx.getMenuButtonBoundingClientRect()
+    this.setData({
+      menuHeight: res.height,
+      menuWidth: res.width,
+      dday: Math.trunc((new Date().getTime()-new Date(2022,6,19).getTime())/86400000)
+      
+    })
+    wx.getSystemInfo({
+      success: res => {
+        this.setData({
+          homeHeight: res.windowHeight,
+          homeWidth: res.windowWidth,
+          statusBarHeight: res.statusBarHeight
+        })
+      }
+    })
+    wx.loadFontFace({
+      family: 'num',
+      source: 'url("https://636c-cloud1-5g71z22k302f6667-1322740242.tcb.qcloud.la/THE-GLITCH.ttf?sign=b79309666bbcdd1c159fa1cc244ffd34&t=1701102360")',
+      scopes: ['webview', 'native'], //由于canvas是原生组件，所以需要加上native，否则无效
+
+      success: function () {}
+    })
+    
+    wx.loadFontFace({
+      family: 'ji',
+      source: 'url("https://636c-cloud1-5g71z22k302f6667-1322740242.tcb.qcloud.la/Rebelion.ttf?sign=74de1913683fb212594d1bd4cec29b43&t=1701101043")',
+      scopes: ['webview', 'native'], //由于canvas是原生组件，所以需要加上native，否则无效
+
+      success: function () {}
+    })
+    wx.loadFontFace({
+      family: 'chi',
+      source: 'url("https://636c-cloud1-5g71z22k302f6667-1322740242.tcb.qcloud.la/ZCOOLXiaoWei-Regular.ttf?sign=b1386f780bbd000fdb53d94bfc8cbd0f&t=1701103524")',
+      scopes: ['webview', 'native'], //由于canvas是原生组件，所以需要加上native，否则无效
+
+      success: function () {}
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
